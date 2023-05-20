7c7894f99e3cc23bae38aa5c52073c9727973ffe
MortInsane <mort.insane@gmail.com>
2023-05-20 22:19:46 +0300

    [Changed]: vars from .env

--- utils/config.py
+++ utils/config.py
@@ -5,4 +5,9 @@ env = Env()
 env.read_env()
 
 
-DJANGO_SECRET = env.str("DJANGO_SECRET")
+DJANGO_SECRET = env.str('DJANGO_SECRET')
+
+POSTGRES_DB = env.str('POSTGRES_DB')
+POSTGRES_USER = env.str('POSTGRES_USER')
+POSTGRES_PASSWORD = env.str('POSTGRES_PASSWORD')
+POSTGRES_PORT = env.str('POSTGRES_PORT')
