27fa523ed3fb182a88f43be9a8ac9d0ee121c113
MortInsane <mort.insane@gmail.com>
2023-05-20 22:15:07 +0300

    [Added]: models accounting & categories

--- accounting/admin.py
+++ accounting/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Accounting, Categories
 
-# Register your models here.
+
+admin.site.register(Accounting)
+admin.site.register(Categories)
+
+
--- accounting/models.py
+++ accounting/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from datetime import date
 
-# Create your models here.
+
+class Categories(models.Model):
+    title = models.CharField(max_length=100)
+
+
+    def __str__(self):
+        return self.title
+
+
+class Accounting(models.Model):
+    income_expense = models.CharField(max_length=7)
+    description = models.CharField(max_length=100)
+    quantity = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ForeignKey(Categories, on_delete=models.SET_NULL, null=True)
+    created_date = models.DateField(default=date.today)
+    updated_date = models.DateTimeField(auto_now_add=False, auto_now=True)
+
+
+    def __str__(self):
+        return self.description
+
